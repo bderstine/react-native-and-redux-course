ea2357542a075e95bca18fdd5734ac4d1a2a81f1
Brad <bderstine@americanbible.org>
2017-05-23 08:30:27 -0400

    add CardSection, update boilerplate

--- src/components/boilerplate-simple.js
+++ src/components/boilerplate-simple.js
@@ -1,6 +1,6 @@
 // If should be "class" based component, or "functional" component
 // If just showing data, prensentational = functional
-// If you need lifecycle methods, access to state = class
+// If you need lifecycle methods, access to state, data fetching = class
 
 // boilerplate functional component
 import React from 'react';
@@ -8,8 +8,26 @@ import { View, Text } from 'react-native';
 
 const AlbumDetail = () => {
 	return (
-		<View></View>
+		<View style={styles.containerStyle}>
+		</View>
 	);
+};
+
+const styles = {
+	containerStyle: {
+		borderWidth: 1,
+		borderRadius: 2,
+		borderColor: '#ddd',
+		borderBottomWidth: 0,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		shadowRadius: 2,
+		elevation: 1,
+		marginLeft: 5,
+		marginRight: 5,
+		marginTop: 10
+	}
 };
 
 export default AlbumDetail;
